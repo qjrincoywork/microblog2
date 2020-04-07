6d3de61f15b228f6ba8c0b80f81ccfb56a28e066
qjrincoywork <quirjohnincoy.work@gmail.com>
2020-04-07 09:11:43 +0800

    email message

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -351,11 +351,11 @@ class UsersController extends AppController {
                             $to = trim($this->request->data['UserProfile']['email']);
                             
                             $message = "Dear <span style='color:#666666'>" . ucwords($name) . "</span>,<br/><br/>";
-                            $message .= "Your account has been created successfully by Administrator.<br/>";
+                            $message .= "Your account has been created successfully.<br/>";
                             $message .= "Please find the below details of your account: <br/><br/>";
                             $message .= "<b>Full Name:</b> " . ucwords($name) . "<br/>";
                             $message .= "<b>Email Address:</b> " . $to . "<br/>";
-                            $message .= "<b>Username:</b> " . $this->data['User']['username'] . "<br/>";
+                            $message .= "<b>Username:</b> " . $this->data['User']['username'] . "<br/><br/>";
                             $message .= "<b>Activate your account by clicking </strong><a href='$activationUrl'>Activate Account now</a></strong></b><br/>";
                             $message .= "<br/>Thanks, <br/>YNS Team";
                             
